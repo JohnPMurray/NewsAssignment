1f8dfa50cdde73ff3f9135d3641c80f9c05d49ef
John P. Murray <jpm7003@rit.edu>
2018-03-28 16:00:04 -0400

    added espn rss

--- newsSite.php
+++ newsSite.php
@@ -1,26 +1,30 @@
 <html>
     <header>
+        
         <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-        <script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js" type="text/javascript"></script>
-        <style type="text/css"> 
-            @import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
-            #feedControl {
-                margin-top : 10px;
-                margin-left: auto;
-                margin-right: auto;
-                width : 440px;
-                font-size: 12px;
-                color: #9CADD0;
-            }
+        <script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js"
+        type="text/javascript"></script>
+
+        <style type="text/css">
+        @import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
+
+        #feedControl {
+        margin-top : 10px;
+        margin-left: auto;
+        margin-right: auto;
+        width : 440px;
+        font-size: 12px;
+        color: #9CADD0;
+        }
         </style>
         <script type="text/javascript">
-            function load() {
-                var feed ="http://feeds.bbci.co.uk/news/world/rss.xml";
-                new GFdynamicFeedControl(feed, "feedControl");
+        function load() {
+        var feed ="http://www.espn.com/espn/rss/news";
+        new GFdynamicFeedControl(feed, "feedControl");
 
-            }
-            google.load("feeds", "1");
-            google.setOnLoadCallback(load);
+        }
+        google.load("feeds", "1");
+        google.setOnLoadCallback(load);
         </script>
         <Title> News World </Title>
         <h1> News World </h1>
@@ -28,8 +32,8 @@
     </header>
     <body>
         <div><h3>News:</h3></div>
-        <div id="feedControl">
-            Loading...
+        <div>
+        <div id="feedControl">Loading...</div>
         </div>
     </body>
 </html>
