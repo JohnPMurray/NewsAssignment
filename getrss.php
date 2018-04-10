8fe2dbe69e68d4ab2b742c76ca4f606277737762
John P. Murray <jpm7003@rit.edu>
2018-04-10 12:17:31 -0400

    debugging

--- getrss.php
+++ getrss.php
@@ -12,18 +12,18 @@ if ($q != "All"){
 for ($j=0; $j < sizeof($docPageList); $j++){
     echo("<h3><b>" . $titles[$j] . "</b></h3>");
     $xmlDoc->load("http://www.espn.com/espn/rss/news");
-    // $x=$xmlDoc->getElementsByTagName('item');
-//     for ($i=0; $i<=10; $i++) {
-//         $item_title=$x->item($i)->getElementsByTagName('title')
-//         ->item(0)->childNodes->item(0)->nodeValue;
-//         $item_link=$x->item($i)->getElementsByTagName('link')
-//         ->item(0)->childNodes->item(0)->nodeValue;
-//         $item_desc=$x->item($i)->getElementsByTagName('description')
-//         ->item(0)->childNodes->item(0)->nodeValue;
-//         echo ("<p><a href='" . $item_link
-//         . "'>" . $item_title . "</a>");
-//         echo ("<br>");
-//         echo ($item_desc . "</p>");
-//     }
+    $x=$xmlDoc->getElementsByTagName('item');
+    for ($i=0; $i<=10; $i++) {
+        $item_title=$x->item($i)->getElementsByTagName('title')
+        ->item(0)->childNodes->item(0)->nodeValue;
+        $item_link=$x->item($i)->getElementsByTagName('link')
+        ->item(0)->childNodes->item(0)->nodeValue;
+        $item_desc=$x->item($i)->getElementsByTagName('description')
+        ->item(0)->childNodes->item(0)->nodeValue;
+        echo ("<p><a href='" . $item_link
+        . "'>" . $item_title . "</a>");
+        echo ("<br>");
+        echo ($item_desc . "</p>");
+    }
 }
 ?>
