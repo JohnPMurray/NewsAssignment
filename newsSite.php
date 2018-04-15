c9181df125cab75786ffaa08ff6373611d9ae538
John P. Murray <jpm7003@rit.edu>
2018-04-15 16:37:41 -0400

    working on adding logout

--- newsSite.php
+++ newsSite.php
@@ -82,7 +82,22 @@ session_start();
                 }
 
             function logout() {
-                <?php $_SESSION["username"]="";?>
+                if (window.XMLHttpRequest) {
+                    // code for IE7+, Firefox, Chrome, Opera, Safari
+                    xmlhttp=new XMLHttpRequest();
+                } else {  // code for IE6, IE5
+                    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                xmlhttp.onreadystatechange=function() {
+                    if (this.readyState==4 && this.status==200) {
+                    }
+                }
+                link = document.getElementById(title).href
+                desc = document.getElementById(title+"-desc").innerHTML
+                date = document.getElementById(title+"-date").innerHTML
+
+                xmlhttp.open("GET", "logout.php", true);
+                xmlhttp.send();
             }
         </script>
         <Title> News World </Title>
